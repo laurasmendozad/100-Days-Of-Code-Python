21f177b86128f29249160078e31cfb4f3d086931
Laura Sofía Mendoza Díaz <laurasofi0507@hotmail.com>
2023-12-25 19:42:55 -0500

    Day 26

--- ProjectsPerDay/Day 026/Project - NATO Alphabet/main.py
+++ ProjectsPerDay/Day 026/Project - NATO Alphabet/main.py
@@ -1,8 +1,8 @@
 ''' NATO Alphabet Project '''
 import pandas as pd
 
-data = pd.read_csv("nato_phonetic_alphabet.csv")
-data_dict = {row[0]: row[1] for (index, row) in data.iterrows()}
+data = pd.read_csv(r"ProjectsPerDay\Day 026\Project - NATO Alphabet\nato_phonetic_alphabet.csv")
+data_dict = {row.iloc[0]: row.iloc[1] for (index, row) in data.iterrows()}
 
 w = input("Enter a word: ")
 w_alph = [data_dict[letter.capitalize()] for letter in w]

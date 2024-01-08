b4309b64f9a6daa185a0087f8860c597ed65cf18
Laura Sofía Mendoza Díaz <laurasofi0507@hotmail.com>
2024-01-07 22:03:27 -0500

    Day 39

---
+++ ProjectsPerDay/Day 039/notification_manager.py
@@ -0,0 +1,22 @@
+''' Notification Manager '''
+import os
+from twilio.rest import Client
+
+TWILIO_SID = os.environ["TWILIO_ACCOUNT_SID"]
+TWILIO_AUTH_TOKEN = os.environ["TWILIO_AUTH_TOKEN"]
+TWILIO_VIRTUAL_NUMBER = '+12015146534'
+TWILIO_VERIFIED_NUMBER = os.environ["PHONE"]
+
+
+class NotificationManager:
+    ''' Notification Manager '''
+    def __init__(self):
+        self.client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+    def send_sms(self, message):
+        ''' Send SMS '''
+        message = self.client.messages.create(
+            body=message,
+            from_=TWILIO_VIRTUAL_NUMBER,
+            to=TWILIO_VERIFIED_NUMBER,
+        )

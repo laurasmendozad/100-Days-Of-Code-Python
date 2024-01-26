59bfd81fe60fbcedc0dbdc3ff26ac5f58787e39c
Laura Sofía Mendoza Díaz <laurasofi0507@hotmail.com>
2024-01-25 22:49:17 -0500

    Day 49

--- ProjectsPerDay/Day 049/dictio = {}.py
+++
@@ -1,8 +0,0 @@
-dictio = {0:"MAIGAASH"}
-try:
-    # Intenta acceder a un elemento del diccionario
-    print(dictio[0])
-
-except KeyError:
-    # La excepción se produce si el diccionario está vacío
-    print("Hello")

25eea6dd527da267840394f5074ed1ad28f5c27b
laurasmendozad <laurasofi0507@hotmail.com>
2024-03-04 10:46:37 -0500

    Day 54

--- ProjectsPerDay/Day 054/Exercise - Python Decorators.py
+++ ProjectsPerDay/Day 054/Exercise - Python Decorators.py
@@ -5,17 +5,17 @@ print(current_time) # seconds since Jan 1st, 1970
 # Write your code below 👇
 
 def speed_calc_decorator(function):
-  t1 = time.time()
-  function()
-  t2 = time.time()
-  print(f"{function.__name__} run speed: {t2-t1}s")
+    t1 = time.time()
+    function()
+    t2 = time.time()
+    print(f"{function.__name__} run speed: {t2-t1}s")
 
 @speed_calc_decorator
 def fast_function():
-  for i in range(1000000):
-    i * i
+    for i in range(1000000):
+        i * i
 
 @speed_calc_decorator
 def slow_function():
-  for i in range(10000000):
-    i * i
+    for i in range(10000000):
+        i * i
